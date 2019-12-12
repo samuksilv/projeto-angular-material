4bbf490bc78ff1beb3815aa18275476a15b3d4f4
samuel <samuel.silv.carvalho@gmail.com>
2019-12-11 22:52:41 -0300

    initialcommit

---
+++ api/Portal.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Portal.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ api/Portal.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Portal.Infra
+{
+    public class Class1
+    {
+    }
+}
